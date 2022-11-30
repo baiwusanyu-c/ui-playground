0097646cf80977907e01a2c8b265cf1f035a3131
baiwusanyu-c <18281682921@qqcom>
2022-11-30 16:07:30 +0800

    chore: 删除注释

--- packages/utils/config.ts
+++ packages/utils/config.ts
@@ -191,17 +191,6 @@ export const defaultConfig: playConfig = {
     },
     // Module 编译后 ✔
     compiledModule: (fileST: typeof fileStore, isSSR: boolean, modules: string[]) => {
-     /* let moduleArr:string[] = []
-      modules.forEach(value => {
-        let val = value.replace('from \'vue\'', 'from \'@vue/runtime-dom\'')
-        val = val.replace('from "vue"', 'from \'@vue/runtime-dom\'')
-        if (isSSR) {
-          val = val.replace('from \'vue/server-renderer\'', 'from \'@vue/server-renderer\'')
-          val = val.replace('from "vue/server-renderer"', 'from \'@vue/server-renderer\'')
-        }
-        moduleArr.push(val)
-      })
-      modules = moduleArr*/
       modules.forEach((value,index) => {
         value = value.replace('from \'vue\'', 'from \'@vue/runtime-dom\'')
         value = value.replace('from "vue"', 'from \'@vue/runtime-dom\'')
@@ -211,15 +200,6 @@ export const defaultConfig: playConfig = {
         }
         modules[index] = value
       })
-     /* let module = modules[0]
-      module = module.replace('from \'vue\'', 'from \'@vue/runtime-dom\'')
-      module = module.replace('from "vue"', 'from \'@vue/runtime-dom\'')
-      if (isSSR) {
-        module = module.replace('from \'vue/server-renderer\'', 'from \'@vue/server-renderer\'')
-        module = module.replace('from "vue/server-renderer"', 'from \'@vue/server-renderer\'')
-      }
-
-      modules[0] = module*/
     },
     // ssr server 、csr、ssr 注入前 ✔
     beforeCreateInject: (fileST: typeof fileStore, isSSR: boolean, modules: string[]) => {
