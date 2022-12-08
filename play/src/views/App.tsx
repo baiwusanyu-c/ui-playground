37b97ff8b5d726d945120c5d971978b5dc5fc7d7
baiwusanyu <740132583@qq.com>
2022-12-08 22:14:44 +0800

    feat: 更新todo

--- play/src/views/App.tsx
+++ play/src/views/App.tsx
@@ -4,6 +4,7 @@ import { PlayGround } from 'ui-playground'
 import { demoEp } from '../../../docs/demo/element-plus/demo-ep'
 const playConfig = demoEp()
 playConfig.useUno = true
+console.log(playConfig)
 const App = () => {
   return (
     <div className="App">
