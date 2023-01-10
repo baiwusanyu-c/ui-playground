d2262bac94d4dcf8d0f0d8ce4f075fe6601ff751
baiwusanyu-c <740132583@qq.com>
2023-01-10 13:59:49 +0800

    chore: update build scripts

--- packages/theme/vite.config.ts
+++ packages/theme/vite.config.ts
@@ -3,25 +3,6 @@ import { defineConfig } from 'vite'
 const pkgPath = resolve(__dirname, 'src/index.scss')
 const themePath = resolve(__dirname, 'src/theme.scss')
 export default defineConfig({
-  plugins: [
-    {
-      name: 'test-plugin',
-      async resolveDynamicImport(specifier, importer) {
-        // console.log(specifier, importer)
-        /* let transBundle:any = {}
-        Object.keys(bundle).forEach(value => {
-          let name = value.split('.')[0] + '.' + value.split('.')[2]
-          transBundle[name] = {
-            ...bundle[value]
-          }
-          transBundle[name].fileName = name
-          transBundle[name].isAsset = true
-        })
-
-        bundle = transBundle */
-      },
-    },
-  ],
   build: {
     target: 'esnext',
     outDir: '../../dist/theme',
